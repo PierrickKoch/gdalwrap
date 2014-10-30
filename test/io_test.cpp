2fd446746bd1e86a41de749a635cf5001b80a13a
Pierrick Koch <pierrick.koch@gmail.com>
2014-10-30 16:12:09 +0100

    [test] clean io test

--- test/io_test.cpp
+++ test/io_test.cpp
@@ -13,10 +13,8 @@ static const uint nloop = 1000;
 static const uint nband = 8;
 static const uint nsx   = 400;
 static const uint nsy   = 400;
-static const std::string name = std::tmpnam(nullptr);
 
-std::ifstream::pos_type filesize(const std::string& filename)
-{
+std::ifstream::pos_type filesize(const std::string& filename) {
     std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
     return in.tellg();
 }
@@ -24,14 +22,19 @@ std::ifstream::pos_type filesize(const std::string& filename)
 void stats(const gdalwrap::gdal& geotif, bool compress) {
     std::chrono::time_point<std::chrono::system_clock> start, end;
     std::chrono::duration<double> elapsed_seconds;
+    std::string name;
+    double size = 0;
     start = std::chrono::system_clock::now();
     for (uint i = 0; i < nloop; i++) {
+        name = std::tmpnam(nullptr);
         geotif.save(name, compress);
+        size += filesize(name) / 1024.0;
+        std::remove( name.c_str() );
     }
     end = std::chrono::system_clock::now();
     elapsed_seconds = end-start;
     std::cout << "gdal::save (x" << nloop << "):  " << elapsed_seconds.count() << "s\n";
-    std::cout << "gdal::save filesize: " << filesize(name) / 1024.0 << " kB\n";
+    std::cout << "gdal::save filesize: " << size / nloop << " kB\n";
 }
 
 void randomize(gdalwrap::gdal& geotif, size_t band,
@@ -59,7 +62,7 @@ int main(int argc, char * argv[]) {
     stats(geotif, true);
 
     randomize(geotif, nband, nsx/2, nsy/2);
-    std::cout << "half random\n";
+    std::cout << "25% random\n";
     std::cout << "compress: off\n";
     stats(geotif, false);
     std::cout << "compress: on\n";
@@ -72,33 +75,33 @@ int main(int argc, char * argv[]) {
     std::cout << "compress: on\n";
     stats(geotif, true);
 
-    std::remove( name.c_str() );
     std::cout << "done." << std::endl;
     return 0;
 }
 
 
 /* result on my setup (i7QM):
+gdalwrap io test...
 empty
 compress: off
-gdal::save (x1000):  16.0366s
+gdal::save (x1000):  15.6806s
 gdal::save filesize: 5004.08 kB
 compress: on
-gdal::save (x1000):  17.6654s
+gdal::save (x1000):  17.6593s
 gdal::save filesize: 34.9512 kB
-half random
+25% random
 compress: off
-gdal::save (x1000):  16.0599s
+gdal::save (x1000):  15.6709s
 gdal::save filesize: 5004.08 kB
 compress: on
-gdal::save (x1000):  41.8193s
+gdal::save (x1000):  42.1742s
 gdal::save filesize: 1152.28 kB
 full
 compress: off
-gdal::save (x1000):  15.7392s
+gdal::save (x1000):  15.7503s
 gdal::save filesize: 5004.08 kB
 compress: on
-gdal::save (x1000):  116.127s
+gdal::save (x1000):  115.614s
 gdal::save filesize: 4505.44 kB
-
+done.
 */
